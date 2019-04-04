3fb6273d41afcd11d72a6c1850343f2bc1c8e449
Roie-github <sup3rmn_21@yahoo.com>
2019-04-04 23:08:41 +0800

    test

---
+++ ServiceApi/Repository/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ServiceApi.Repository
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmpCode { get; set; }
+        public string Mobile { get; set; }
+    }
+}
